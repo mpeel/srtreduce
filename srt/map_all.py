343229a937ca0140f06183fc83b2125c6141197d
Mike Peel <email@mikepeel.net>
2020-10-14 10:18:45 +0100

    +

--- srt/map_all.py
+++ srt/map_all.py
@@ -39,6 +39,7 @@ max_ra = 60.0
 min_dec = 27.5
 max_dec = 36.0
 # res = 0.4 / 60.0
+#res = 1.0 / 60.0
 res = 3.0 / 60.0
 npix_ra = int((max_ra-min_ra)/res)
 npix_dec = int((max_dec-min_dec)/res)
@@ -52,96 +53,119 @@ ras = np.arange(min_ra,max_ra+pixsize_ra,pixsize_ra)
 # max_dec = np.max(dec)
 pixsize_dec = res#(max_dec-min_dec)/(numpix-1)
 decs = np.arange(min_dec,max_dec+pixsize_dec,pixsize_dec)
-maparr = np.zeros([npix_ra,npix_dec])
-weightarr = np.zeros([npix_ra,npix_dec])
 
-# hdr = fits.header.Header()
-# fits.writeto('test_image.fits', maparr, hdr, overwrite=True) 
-# exit()
+# Set 8 to do a combined one at the maxend-end
+# numhorns = 8
+numhorns = 1
+for h in range(0,numhorns):
 
-ext = '.fits'
-numext = 1
-inputlist = os.listdir('/Users/mpeel/Documents/git/astrocode/srt')
-filelist = [f for f in inputlist if ext in f]
-print(filelist)
-filelist.sort()
-print(filelist)
-i = 0
+	maparr = np.zeros([npix_ra,npix_dec])
+	weightarr = np.zeros([npix_ra,npix_dec])
+	hitarr = np.zeros([npix_ra,npix_dec])
+	# hdr = fits.header.Header()
+	# fits.writeto('test_image.fits', maparr, hdr, overwrite=True) 
+	# exit()
 
-
-
-for file in filelist:
-	print(file)
-	if 'map.fits' not in file:
-		inputfits = fits.open(file)
-		cols = inputfits[1].columns
-		print(cols.info())
-
-		ra = inputfits[1].data.field(0)
-		dec = inputfits[1].data.field(1)
-		timestream = inputfits[1].data.field(2)
-		print(ra)
-		print(dec)
-		print(timestream)
-		print(len(ra))
-
-		timestream = np.asarray(subtractbaseline(timestream))
-		# plt.plot(timestream)
-		# plt.savefig(prefix+'_timestream_sub.png')
-		# plt.clf()
-
-		# Remove glitches
-		threshold=5.0
-		std = np.std(timestream)
-		median = np.median(timestream)
-		# print(std)
-		timestream[np.abs(timestream-median) > std*threshold] = 0.0
-		# Do this twice.
-		std = np.std(timestream)
-		median = np.median(timestream)
-		# print(std)
-		timestream[np.abs(timestream-median) > std*threshold] = 0.0
-		# timestream[timestream-median < std/threshold] = 1.0
-		# plt.plot(timestream)
-		# plt.savefig(prefix+'_timestream_sub_deglitch.png')
-		# plt.clf()
-
-		minstart=0
-		maxend=len(timestream-1)
-		for i in range(len(timestream)):
-			start=i-15
-			end=i+15
-			if start < minstart:
-				end += minstart-start
-				start=minstart
-			if end > maxend:
-				start -= maxend-end
-				end=maxend
-			variance = np.var(timestream[start:end])
-			if variance == 0:
-				variance = 1.0
-			if ~np.isfinite(variance):
-				variance=1.0
-			pix_ra = int((ra[i]-min_ra)/pixsize_ra)
-			pix_dec = int((dec[i]-min_dec)/pixsize_dec)
-			# print(pix_ra,pix_dec)
-			maparr[pix_ra,pix_dec] += timestream[i]/variance
-			weightarr[pix_ra,pix_dec] += 1.0/variance
-
-maparr[weightarr != 0] /= weightarr[weightarr != 0]
-
-# Write out maps
-hdr = fits.header.Header()
-fits.writeto('combined_map.fits', maparr, hdr, overwrite=True) 
-fits.writeto('combined_weights_map.fits', weightarr, hdr, overwrite=True) 
+	# if h == 7:
+	ext = '.fits'
+	# else:
+		# ext = 'fits'+str(h)
+	numext = 1
+	basedir = '/Volumes/Toshiba5TB2/SRT/28-19/reduce/'
+	basedir = '/Volumes/Toshiba5TB2/SRT/28-19/calibrated/tod/'
+	inputlist = os.listdir(basedir+'tods/')
+	filelist = [f for f in inputlist if ext in f]
+	# print(filelist)
+	filelist.sort()
+	print(filelist)
+	i = 0
 
 
-# maparr[weightarr==0] = 1.0
-# print(maparr)
-plt.pcolormesh(ras,decs,maparr.transpose())
-plt.savefig('combined_map.png')
-plt.pcolormesh(ras,decs,weightarr.transpose())
-plt.savefig('combined_weights.png')
-plt.clf()
+
+	for file in filelist:
+		print(file)
+		# if '018' in file:
+			# break
+		if 'map.fits' not in file:
+			inputfits = fits.open(basedir+'tods/'+file)
+			cols = inputfits[1].columns
+			# print(cols.info())
+
+			ra = inputfits[1].data.field(0)
+			dec = inputfits[1].data.field(1)
+			timestream = inputfits[1].data.field(2)
+			# print(np.median(ra)*60.0)
+			# print(dec)
+			# print(timestream)
+			# print(len(ra))
+
+			# timestream = np.asarray(subtractbaseline(timestream))
+			# plt.plot(timestream)
+			# plt.savefig(prefix+'_timestream_sub.png')
+			# plt.clf()
+
+			# Remove glitches
+			threshold=5.0
+			std = np.std(timestream)
+			median = np.median(timestream)
+			# print(std)
+			timestream[np.abs(timestream-median) > std*threshold] = 0.0
+			# Do this twice.
+			std = np.std(timestream)
+			median = np.median(timestream)
+			# print(std)
+			timestream[np.abs(timestream-median) > std*threshold] = 0.0
+			# timestream[timestream-median < std/threshold] = 1.0
+			# plt.plot(timestream)
+			# plt.savefig(prefix+'_timestream_sub_deglitch.png')
+			# plt.clf()
+
+			minstart=0
+			maxend=len(timestream-1)
+			for i in range(len(timestream)):
+				# start=i-15
+				# end=i+15
+				# if start < minstart:
+				# 	end += minstart-start
+				# 	start=minstart
+				# if end > maxend:
+				# 	start -= maxend-end
+				# 	end=maxend
+				start = 0
+				end = len(timestream)
+				variance = np.var(timestream[start:end])
+				if variance == 0:
+					variance = 1.0
+				if ~np.isfinite(variance):
+					variance=1.0
+				pix_ra = int((ra[i]-min_ra)/pixsize_ra)
+				pix_dec = int((dec[i]-min_dec)/pixsize_dec)
+				# print(pix_ra,pix_dec)
+				maparr[pix_ra,pix_dec] += timestream[i]/variance
+				weightarr[pix_ra,pix_dec] += 1.0/variance
+				hitarr[pix_ra,pix_dec] += 1
+
+	maparr[weightarr != 0] /= weightarr[weightarr != 0]
+	maparr[hitarr == 0] = np.nan
+	weightarr[hitarr == 0] = np.nan
+	hitarr[hitarr == 0] = np.nan
+	# Write out maps
+	hdr = fits.header.Header()
+	fits.writeto(basedir+'combined_map'+str(h)+'.fits', maparr, hdr, overwrite=True) 
+	fits.writeto(basedir+'combined_weights_map'+str(h)+'.fits', weightarr, hdr, overwrite=True) 
+	fits.writeto(basedir+'hitmap'+str(h)+'.fits', hitarr, hdr, overwrite=True) 
+
+
+	# maparr[weightarr==0] = 1.0
+	# print(maparr)
+	plt.pcolormesh(ras,decs,maparr.transpose())
+	plt.savefig(basedir+'combined_map_img'+str(h)+'.png')
+	plt.clf()
+	plt.pcolormesh(ras,decs,weightarr.transpose())
+	plt.savefig(basedir+'combined_weights_img'+str(h)+'.png')
+	plt.clf()
+	plt.pcolormesh(ras,decs,hitarr.transpose())
+	plt.savefig(basedir+'hitmap_img'+str(h)+'.png')
+	plt.clf()
 
 
--- srt/read_srt_data.py
+++ srt/read_srt_data.py
@@ -4,84 +4,24 @@ import matplotlib.pyplot as plt
 import astropy.io.fits as fits
 import os
 from scipy.optimize import curve_fit
-
-def linfit(x, A, B):
-	return A*x+B
-
-def subtractbaseline(data, option=0, navg=100):
-	# Crude baseline removal
-	npoints = len(data)
-	# print(npoints)
-	for i in range(0,npoints//navg):
-		start = i*navg
-		end = ((i+1)*navg)
-		# print('start:' + str(start) + ', end: ' + str(end))
-		if option == 0:
-			data[start:end] = data[start:end] - np.median(data[start:end])
-		else:
-			xline = range(0,len(data[start:end]))
-			if len(xline) != 0:
-				A,B=curve_fit(linfit,xline,data[start:end])[0]
-				# print(A,B)
-				data[start:end] = data[start:end] - (A*xline+B)
-
-	if option == 0:
-		data[(npoints//navg)*navg-1:] = data[(npoints//navg)*navg-1:] - np.median(data[(npoints//navg)*navg-1:])
-	else:
-		xline = range(0,len(data[(npoints//navg)*navg-1:]))
-		if len(xline) != 0:
-			A,B=curve_fit(linfit,xline,data[(npoints//navg)*navg-1:])[0]
-			# print(A,B)
-			data[(npoints//navg)*navg-1:] = data[(npoints//navg)*navg-1:] - (A*xline+B)
-
-	return data
+from srt_functions import *
 
 
-def read_fits_file(filename):
-	print(filename)
-	inputfits = fits.open(filename)
-	print(inputfits.info())
-	# print(inputfits[0].columns.names)
-	print(inputfits[1].columns.names)
-	print(inputfits[2].columns.names)
-	print(inputfits[3].columns.names)
-	print(inputfits[4].columns.names)
-	print(inputfits[5].columns.names)
-	print(inputfits[2].data)
-	print(inputfits[3].data)
-	print(inputfits[4].data.field(3)[0])
-	# exit()
-	ra = np.degrees(inputfits[4].data.field(3))
-	dec = np.degrees(inputfits[4].data.field(4))
-	az = np.degrees(inputfits[4].data.field(1))
-	el = np.degrees(inputfits[4].data.field(2))
-	# ra = np.degrees(inputfits[4].data.field(1))
-	# dec = np.degrees(inputfits[4].data.field(2))
-	# az = np.degrees(inputfits[4].data.field(3))
-	# el = np.degrees(inputfits[4].data.field(4))
-	data = inputfits[4].data.field(-1)
-	# exit()
-	# cols = inputfits[0].columns
-	# col_names = cols.names
-	# print(col_names)
-	# nmaps = len(cols)
-	# maps = []
-	# if usehealpixfits:
-	# 	maps = hp.read_map(indir+inputfile,field=None)
-	# else:
-	# 	for i in range(0,nmaps):
-	# 		maps.append(inputfits[1].data.field(i))
-	# 		print(len(maps[i]))
-	# print(len(maps[0]))
-	# # Check to see whether we have nested data, and switch to ring if that is the case.
-	# if (inputfits[1].header['ORDERING'] == 'NESTED'):
-	# 	maps = hp.reorder(maps,n2r=True)
-	# newheader = inputfits[1].header.copy(strip=False)
-	# inputfits.close()
-	return ra, dec, az, el, data
-
-basedir = '/Volumes/Toshiba5TB2/SRT/28-19/modified/L-Band/'
+basedir = '/Volumes/Toshiba5TB2/SRT/28-19/modified/K-Band/'
 # basedir = '/Volumes/Maxtor4TB/SRT/33-18/'
+outdir = '/Volumes/Toshiba5TB2/SRT/28-19/reduce/'
+plotdir = outdir + 'plots/'
+toddir = outdir + 'tods/'
+mapdir = outdir + 'maps/'
+os.makedirs(outdir, exist_ok=True)
+os.makedirs(plotdir, exist_ok=True)
+os.makedirs(toddir, exist_ok=True)
+os.makedirs(mapdir, exist_ok=True)
+
+doplots = False
+
+birdies = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 107, 108, 109, 110, 111, 128, 256, 384, 512, 513, 640, 768, 896, 972, 1023, 1024]
+numchans = 1024
 folderlist = os.listdir(basedir)
 todolist = []
 for folder in folderlist:
@@ -98,11 +38,6 @@ for folder in folderlist:
 		print(subfolderlist)
 print(folderlist)
 print(todolist)
-# inputdir='/Volumes/Maxtor4TB/SRT/28-19/20191205/20191205-224502-28-19-PERSEUS_AZ'
-# prefix='perseus_20191205-224502'
-# inputdir='/Volumes/Maxtor4TB/SRT/28-19/20191205/20191205-195834-28-19-PERSEUS_AZ'
-# prefix='perseus_20191205-195834'
-
 for inputdir in todolist:
 	prefix=inputdir.replace(basedir,'').replace('/','_')
 	ext = 'fits'
@@ -116,79 +51,107 @@ for inputdir in todolist:
 			continue
 		for i in range(numext):
 			az_set, el_set, ra_set, dec_set, data = read_fits_file(inputdir+'/'+filename[:])#+str(i))
+			horn_number = int(filename[-1])
+			nanarr = np.zeros(len(az_set))
+			nanarr[:] = np.nan
 			# Just look at the first 1k channels for now - removing the first 10 channels as bad.
+			# Ordering is LL, RR, U, Q
+			# Only using LL, RR for now
+			data = data[:,0:2048]
 			print(np.shape(data))
-			# exit()
 			# data = np.asarray(data[:,10:1023])#2048*2-1]
 			# data = data[:,8000:8300]
 			# Blank the first N channels
 			# data[:,1024:1034] = 1.0
-			data[:,190:192] = 1.0#np.nan
-			data[:,226:308] = 1.0#np.nan
-			data[:,410:483] = 1.0#np.nan
-			data[:,505:534] = 1.0#np.nan
-			data[:,580:580] = 1.0#np.nan
-			data[:,715:715] = 1.0#np.nan
-			data[:,761:778] = 1.0#np.nan
-			data[:,812:814] = 1.0#np.nan
+			# print(np.nanargmax(np.nanmean(data,axis=0)))
+			for birdie in birdies:
+				data[:,birdie-1:birdie][:] = np.nan
+				data[:,numchans+birdie-1:numchans+birdie] = np.nan
+				# data[:,2*numchans+birdie-1:2*numchans+birdie] = 0.0#np.nan
+				# data[:,3*numchans+birdie-1:3*numchans+birdie] = 0.0#np.nan
+			# if horn_number == 2:
+			data[:,0:60] = np.nan
+			data[:,1024:1024+60] = np.nan
+			# Horn 5 LL is missing
+			if horn_number == 5:
+				data[:,0:1024] = np.nan
+			# Horns 3 and 6 RR is weird
+			if horn_number == 3 or horn_number == 6:
+				data[:,1024:] = np.nan
+			# data[:,190:192] = 1.0#np.nan
+			# data[:,226:308] = 1.0#np.nan
+			# data[:,410:483] = 1.0#np.nan
+			# data[:,505:534] = 1.0#np.nan
+			# data[:,580:580] = 1.0#np.nan
+			# data[:,715:715] = 1.0#np.nan
+			# data[:,761:778] = 1.0#np.nan
+			# data[:,812:814] = 1.0#np.nan
 			if trip == 0:
 				print(np.shape(data))
 			# data[~np.isfinite(data)] = 0.0
-			if trip == 0:
-				plt.plot(np.nanmean(data,axis=0),'b', label='Mean in given freq bin')
-				plt.savefig(prefix+'_bandpass.png')
+			# if trip == 0:
+			# 	plt.ylim(np.nanmin(data),np.nanmax(data))
+			# 	plt.plot(np.nanmean(data,axis=0),'b', label='Mean in given freq bin')
+			# 	plt.savefig(plotdir+filename+'_bandpass.png')
+			# 	plt.clf()
+			# 	plt.pcolormesh(data)
+			# 	plt.savefig(plotdir+prefix+'_data_waterfall.png')
+			# 	plt.clf()
+			# plotdata = data.copy()
+			# plotdata[~np.isfinite(plotdata)] = 0.0
+			# print(np.nanmin(plotdata))
+			if doplots:
+				plt.plot(np.nanmean(data,axis=0),'b.', label='Mean in given freq bin')
+				plt.savefig(plotdir+filename+'_bandpass.png')
 				plt.clf()
 				plt.pcolormesh(data)
-				plt.savefig(prefix+'_data_waterfall.png')
+				plt.savefig(plotdir+filename+'_data_waterfall.png')
 				plt.clf()
-			plt.plot(np.nanmean(data,axis=0),'b', label='Mean in given freq bin')
-			plt.savefig(prefix+'_bandpass_'+filename[:-5]+'.png')
-			plt.clf()
-			plt.pcolormesh(data)
-			plt.savefig(prefix+'_data_waterfall_'+filename[:-5]+'.png')
-			plt.clf()
 
-			# Remove bad values in the bandpass
-			threshold=3.0
-			bandpass = np.nanmean(data,axis=0)
-			# for test in range(0,len(bandpass)):
-			# 	print(str(test) + ' ' + str(bandpass[test]))
-			# exit()
-			std = np.nanstd(bandpass)
-			median = np.nanmedian(bandpass)
-			# print(std)
-			bandpass[np.abs(bandpass-median) > std*threshold] = np.nan
-			# Do this twice.
-			std = np.nanstd(bandpass)
-			median = np.nanmedian(bandpass)
-			# print(std)
-			bandpass[np.abs(bandpass-median) > std*threshold] = np.nan
-			# timestream[timestream-median < std/threshold] = 1.0
-			if trip == 0:
-				plt.plot(bandpass)
-				plt.savefig(prefix+'_bandpass_deglitch.png')
-				plt.clf()
-			# Flag based on differencing as well
-			diff = np.abs(np.diff(bandpass,append=1))
-			# diff = np.concatenate(np.asarray(diff), np.asarray([1000]))
-			bandpass[diff>5] = np.nan
-			for j in range(0,len(bandpass)-1):
-				if bandpass[j+1] == np.nan:
-					bandpass[j] = np.nan
+			# # Remove bad values in the bandpass
+			# threshold=3.0
+			# bandpass = np.nanmean(data,axis=0)
+			# # for test in range(0,len(bandpass)):
+			# # 	print(str(test) + ' ' + str(bandpass[test]))
+			# std = np.nanstd(bandpass)
+			# median = np.nanmedian(bandpass)
+			# # print(std)
+			# bandpass[np.abs(bandpass-median) > std*threshold] = np.nan
+			# # Do this twice.
+			# std = np.nanstd(bandpass)
+			# median = np.nanmedian(bandpass)
+			# # print(std)
+			# bandpass[np.abs(bandpass-median) > std*threshold] = np.nan
+			# # timestream[timestream-median < std/threshold] = 1.0
+			# if trip == 0:
+			# 	plt.plot(bandpass)
+			# 	plt.savefig(plotdir+prefix+'_bandpass_deglitch.png')
+			# 	plt.clf()
+			# # Flag based on differencing as well
+			# diff = np.abs(np.diff(bandpass,append=1))
+			# # diff = np.concatenate(np.asarray(diff), np.asarray([1000]))
+			# bandpass[diff>5] = np.nan
+			# for j in range(0,len(bandpass)-1):
+			# 	if bandpass[j+1] == np.nan:
+			# 		bandpass[j] = np.nan
 
-			if trip == 0:
-				plt.plot(bandpass)
-				plt.savefig(prefix+'_bandpass_deglitch2.png')
-				plt.clf()
-			badvals = np.isnan(bandpass)
-			print(badvals==True)
-			print(np.sum(badvals==True))
-			data[:,badvals==True] = data[:,badvals==True]*np.nan
+			# if trip == 0:
+			# 	plt.plot(bandpass)
+			# 	plt.savefig(plotdir+prefix+'_bandpass_deglitch2.png')
+			# 	plt.clf()
+			# badvals = np.isnan(bandpass)
+			# print(badvals==True)
+			# print(np.sum(badvals==True))
+			# data[:,badvals==True] = data[:,badvals==True]*np.nan
 
-			data = data/data.mean(axis=0)
-			if trip == 0:
+			data = data/np.nanmean(data,axis=0)
+			maxval = np.nanargmax(np.nanstd(data,axis=0))
+			if maxval > 1024:
+				maxval -= 1024
+			print(maxval)
+			if trip == 0 and doplots:
 				plt.pcolormesh(data)
-				plt.savefig(prefix+'_data_waterfall_bp.png')
+				plt.savefig(plotdir+filename+'_data_waterfall_bp.png')
 				plt.clf()
 				print(np.nanmin(data))
 				print(np.nanmax(data))
@@ -196,37 +159,45 @@ for inputdir in todolist:
 			if trip == 0:
 				print(std)
 			data[np.abs(data) > 5.0*std] = 0.0
-			if trip == 0:
+			if doplots:
 				plt.pcolormesh(data)
-				plt.savefig(prefix+'_data_waterfall_bp_std.png')
+				plt.savefig(plotdir+filename+'_data_waterfall_bp_std.png')
 				plt.clf()
-			plt.pcolormesh(data)
-			plt.savefig(prefix+'_data_waterfall_bp_std_'+filename[:-5]+'.png')
-			plt.clf()
+				plt.plot(np.nanstd(data,axis=0),'b.', label='Mean in given freq bin')
+				plt.savefig(plotdir+filename+'_bandpass_flat.png')
+				plt.clf()
 			# plt.plot(data)
 			# plt.savefig('test_data.png')
 			# plt.clf()
 			# Now flatten the dataset
 			timestream_set = np.nanmean(data,axis=1)
-			if trip == 0:
+			# And remove a linear trend
+			timestream_set = subtractbaseline(timestream_set,option=1,navg=len(timestream_set))
+			# if trip == 0:
+			if doplots:
 				plt.plot(timestream_set)
-				plt.savefig(prefix+'_timestream.png')
+				plt.savefig(plotdir+filename+'_timestream.png')
 				plt.clf()
-			exit()
-			print(np.shape(az_set))
+				print(np.shape(az_set))
+
+			# Write out the TOD to disk
+			writefits(toddir+filename.replace('.','_')+'_tod.fits', ra_set, dec_set, timestream_set,prefix)
+
 			if trip == 0:
-				az = list(az_set.copy())
-				el = list(el_set.copy())
+				# az = list(az_set.copy())
+				# el = list(el_set.copy())
 				ra = list(ra_set.copy())
 				dec = list(dec_set.copy())
 				timestream = list(timestream_set.copy())
 				trip = 1
 			else:
-				az = az + list(az_set)
-				el = el + list(el_set)
+				# az = az + list(az_set)
+				# el = el + list(el_set)
 				ra = ra + list(ra_set)
 				dec = dec + list(dec_set)
 				timestream = timestream + list(timestream_set)
+		# 	break
+		# break
 	print(np.shape(ra))
 	# plt.xlim([50,60])
 	# plt.ylim([27,36])
@@ -234,96 +205,81 @@ for inputdir in todolist:
 	plt.plot(ra, dec,'b,')
 	plt.xlabel('RA [deg]')
 	plt.ylabel('Dec [deg]')
-	plt.savefig(prefix+'_ra_dec.png')
+	plt.savefig(plotdir+prefix+'_ra_dec.png')
 	plt.clf()
 	# plt.plot(az)
 	# plt.ylabel('Azimuth [Deg]')
 	# plt.xlabel('Sample')
 	# plt.savefig(prefix+'_az.png')
 	# plt.clf()
-	plt.plot(az, el,'g,')
-	plt.xlabel('Azimuth')
-	plt.ylabel('Elevation')
-	plt.savefig(prefix+'_az_el.png')
-	plt.clf()
-	plt.plot(timestream)
-	plt.savefig(prefix+'_timestream.png')
-	plt.clf()
-	np.savetxt(prefix+'_radec.txt',np.transpose([ra, dec]))
-	print('Hi')
+	# plt.plot(az, el,'g,')
+	# plt.xlabel('Azimuth')
+	# plt.ylabel('Elevation')
+	# plt.savefig(plotdir+prefix+'_az_el.png')
+	# plt.clf()
+	# plt.plot(timestream)
+	# plt.savefig(plotdir+prefix+'_timestream.png')
+	# plt.clf()
+	# np.savetxt(plotdir+prefix+'_radec.txt',np.transpose([ra, dec]))
+	# print('Hi')
+
+	# # Create a mask of the different azimuth scans
+	# az_mask = np.zeros(len(az)).astype(int)
+	# # Find out whether we're going up or down in elevation
+	# for i in range(1,len(az_mask)):
+	# 	if az[i-1] < az[i] or az[i-1] > az[i]+1.0:
+	# 		az_mask[i] = -1
+	# 	else:
+	# 		az_mask[i] = 1
+
+	# # To catch noisy bits in transitions, require that sets of testlen all have to have the same value
+	# testlen = 10
+	# for i in range(0,int(len(az_mask)/testlen)-testlen):
+	# 	if np.sum(az_mask[i*testlen:i*testlen+testlen]) != testlen and np.sum(az_mask[i*testlen:i*testlen+testlen]) != -testlen:
+	# 		az_mask[i*testlen:i*testlen+testlen] = 0
+	# # Count how many different sections we have, and update the mask so we can extract them.
+	# num_azscans = 1
+	# notzero = 0
+	# for i in range(0,len(az_mask)):
+	# 	if az_mask[i] != 0:
+	# 		az_mask[i] = az_mask[i] * num_azscans
+	# 		notzero = 1
+	# 	else:
+	# 		if notzero == 1:
+	# 			num_azscans = num_azscans+1
+	# 			notzero = 0
+	# # print(num_azscans)
+	# plt.plot(az_mask)
+	# plt.savefig(prefix+'_azmask.png')
+	# plt.clf()
+	# az_mask2 = np.abs(az_mask.copy())
+	# plt.plot(az_mask2)
+	# plt.savefig(prefix+'_azmask2.png')
+	# plt.clf()
+
+	# timestream = np.asarray(timestream)
+	# for i in range(1,np.max(az_mask2)+1):
+	# 	# Destripe per azimuth strip
+	# 	xline=np.arange(0,sum(az_mask2==i))
+	# 	# print(timestream[az_mask==i])
+	# 	ret=curve_fit(linfit,xline,timestream[az_mask2==i])
+	# 	# print(ret)
+	# 	A=ret[0][0]
+	# 	B=ret[0][1]
+	# 	# print(A,B)
+	# 	timestream[az_mask2==i] = timestream[az_mask2==i] - (A*xline+B)
+	# # Flag the change-over points.
+	# timestream[az_mask==0]=0.0
 
 
-	# Write out the TOD to disk before destriping
-	ra_col = fits.Column(name='ra',format='D',array=np.asarray(ra))
-	dec_col = fits.Column(name='dec',format='D',array=np.asarray(dec))
-	col1 = fits.Column(name='ch1',format='D',array=np.asarray(timestream))
-	cols = fits.ColDefs([ra_col, dec_col, col1])
-	hdu = fits.BinTableHDU.from_columns(cols)
-
-	hdr = fits.Header()
-	hdr['INSTRUM'] = 'SRT-PERS'
-	hdr['VERSION'] = '0.0'
-	hdr['OBSNAME'] = prefix
-	primary_hdu = fits.PrimaryHDU(header=hdr)
-	hdul = fits.HDUList([primary_hdu, hdu])
-	hdul.writeto(prefix+'_tod.fits',overwrite=True)
-
-# 	# Create a mask of the different azimuth scans
-# 	az_mask = np.zeros(len(az)).astype(int)
-# 	# Find out whether we're going up or down in elevation
-# 	for i in range(1,len(az_mask)):
-# 		if az[i-1] < az[i] or az[i-1] > az[i]+1.0:
-# 			az_mask[i] = -1
-# 		else:
-# 			az_mask[i] = 1
-
-# 	# To catch noisy bits in transitions, require that sets of testlen all have to have the same value
-# 	testlen = 10
-# 	for i in range(0,int(len(az_mask)/testlen)-testlen):
-# 		if np.sum(az_mask[i*testlen:i*testlen+testlen]) != testlen and np.sum(az_mask[i*testlen:i*testlen+testlen]) != -testlen:
-# 			az_mask[i*testlen:i*testlen+testlen] = 0
-# 	# Count how many different sections we have, and update the mask so we can extract them.
-# 	num_azscans = 1
-# 	notzero = 0
-# 	for i in range(0,len(az_mask)):
-# 		if az_mask[i] != 0:
-# 			az_mask[i] = az_mask[i] * num_azscans
-# 			notzero = 1
-# 		else:
-# 			if notzero == 1:
-# 				num_azscans = num_azscans+1
-# 				notzero = 0
-# 	# print(num_azscans)
-# 	plt.plot(az_mask)
-# 	plt.savefig(prefix+'_azmask.png')
-# 	plt.clf()
-# 	az_mask2 = np.abs(az_mask.copy())
-# 	plt.plot(az_mask2)
-# 	plt.savefig(prefix+'_azmask2.png')
-# 	plt.clf()
-
-	timestream = np.asarray(timestream)
-# 	for i in range(1,np.max(az_mask2)+1):
-# 		# Destripe per azimuth strip
-# 		xline=np.arange(0,sum(az_mask2==i))
-# 		# print(timestream[az_mask==i])
-# 		ret=curve_fit(linfit,xline,timestream[az_mask2==i])
-# 		# print(ret)
-# 		A=ret[0][0]
-# 		B=ret[0][1]
-# 		# print(A,B)
-# 		timestream[az_mask2==i] = timestream[az_mask2==i] - (A*xline+B)
-# 	# Flag the change-over points.
-# 	timestream[az_mask==0]=0.0
-
-
-# # 	timestream = np.asarray(subtractbaseline(timestream))
-# 	plt.plot(timestream)
-# 	plt.savefig(prefix+'_timestream_sub.png')
-# 	plt.clf()
+	# # timestream = np.asarray(subtractbaseline(timestream))
+	# plt.plot(timestream)
+	# plt.savefig(prefix+'_timestream_sub.png')
+	# plt.clf()
 
 	# Remove glitches
 	threshold=5.0
+	timestream = np.asarray(timestream, dtype=np.float64)
 	std = np.nanstd(timestream)
 	median = np.nanmedian(timestream)
 	# print(std)
@@ -335,17 +291,16 @@ for inputdir in todolist:
 	timestream[np.abs(timestream-median) > std*threshold] = np.nan
 	# timestream[timestream-median < std/threshold] = 1.0
 	plt.plot(timestream)
-	plt.savefig(prefix+'_timestream_sub_deglitch.png')
+	plt.savefig(plotdir+prefix+'_timestream_sub_deglitch.png')
 	plt.clf()
 
 
-	numpix = 75
+	numpix = 250
 	min_ra = np.min(ra)
 	max_ra = np.max(ra)
 	pixsize_ra = (max_ra-min_ra)/(numpix-1)
 	ras = np.arange(min_ra,max_ra+pixsize_ra,pixsize_ra)
 	# print(len(ras))
-	# exit()
 	min_dec = np.min(dec)
 	max_dec = np.max(dec)
 	pixsize_dec = (max_dec-min_dec)/(numpix-1)
@@ -360,15 +315,12 @@ for inputdir in todolist:
 		weightarr[pix_ra,pix_dec] += 1
 
 	maparr[weightarr != 0] /= weightarr[weightarr != 0]
-	maparr[weightarr==0] = 1.0
+	maparr[weightarr==0] = np.nan
 	# print(maparr)
 	plt.pcolormesh(ras,decs,maparr.transpose())
-	plt.savefig(prefix+'_map.png')
+	plt.savefig(mapdir+prefix+'_map.png')
 	plt.clf()
 
 	hdr = fits.header.Header()
-	fits.writeto(prefix+'_map.fits', maparr, hdr, overwrite=True) 
-	fits.writeto(prefix+'_weights_map.fits', weightarr, hdr, overwrite=True) 
-
-	# exit()
-
+	fits.writeto(mapdir+prefix+'_map.fits', maparr, hdr, overwrite=True) 
+	fits.writeto(mapdir+prefix+'_weights_map.fits', weightarr, hdr, overwrite=True) 
